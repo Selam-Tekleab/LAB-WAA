b7326a9d17102c87e991695e96ac2851628f902d
selam-Tekleab <selam.tekleab6@gmail.com>
2024-10-01 08:03:21 -0500

    bookController added

--- src/main/java/com/example/lab1/Test.java
+++ src/main/java/com/example/lab1/Test.java
@@ -8,7 +8,7 @@ public class Test {
     public ArrayList<Book> getBooks() {
 
         ArrayList<Book> B = new ArrayList<Book>();
-        B.add(new Book(1, "Database", "411658", 357));
+        B.add(new Book(1, "Database", "411658", 359));
         B.add(new Book(2, "SoftwareEngineering", "405858", 550));
         B.add(new Book(3, "WebApplication", "460125", 850));
         B.add(new Book(4, "SoftwareArchitecture", "169853", 746));
